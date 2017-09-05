2317a9a181a3421656da13fedee5aaa43a36491f
Benjamin Stephens <bs@NCC1776.local>
2017-09-05 16:15:21 -0700

    Implements collision detection

--- client/watchout.js
+++ client/watchout.js
@@ -27,6 +27,7 @@
 //collision handling:
   //if collision, reset score
 */
+var currentTime = 1;
 
 var options = {
   width: 700,
@@ -39,12 +40,26 @@ class Enemy {
     this.x = Math.random() * options.width;
     this.y = Math.random() * options.height;
     this.r = 12;
+    this.lastx = 0;
+    this.lasty = 0;
   }
   newX() {
-    return Math.random() * options.width;
+    this.lastx = this.x;
+    this.x = Math.random() * options.width;
+    return this.x;
+  }
+  diffX() {
+    let dx = (this.x - this.lastx) * (currentTime / 1000);
+    return this.lastx + dx;
   }
   newY() {
-    return Math.random() * options.height;
+    this.lasty = this.y;
+    this.y = Math.random() * options.height;
+    return this.y;
+  }
+  diffY() {
+    let dy = (this.y - this.lasty) * (currentTime / 1000);
+    return this.lasty + dy;
   }
 }
 
@@ -107,17 +122,66 @@ var d3player = board.selectAll('circle.player')
   .attr('r', (player) => player.r)
   .style('fill', 'red');
 
+var collisionHandle = () => {
+
+};
+
 setInterval(() => {
   d3Enemies.transition()
     .duration(1000)
+    // .tween('move', () => {
+    //   var enemy = d3.select(this);
+    //
+    //   var x1 = enemy.attr('cx');
+    //   var y1 = enemy.attr('cy');
+    //
+    //   var x2 = Math.random() * options.width;
+    //   var y2 = Math.random() * options.height;
+    //
+    //   return (time) => {
+    //     dx = x1 + x2 * time;
+    //     dy = y1 + y2 * time;
+    //
+    //     enemy.attr('cx', dx).attr('cy', dy);
+    //   }
+    // })
     .attr('cx', (enemy) => enemy.newX())
     .attr('cy', (enemy) => enemy.newY());
+  // .ease('linear');
 }, 1000);
-//console.log(JSON.stringify(generateEnemies()));
 
-// d3.event('drag', (d) => {
-//   console.log(d3.mouse);
-//   d.attr('cx', (player) => d3.mouse[0])
-//     .attr('cy', (player) => d3.mouse[1]);
-// })
-//.append('svg:svg').attr('width', options.width).attr('height', options.height).css('color', 'red');
+setInterval(() => {
+  currentTime += 10;
+  if (currentTime > 1000) {
+    currentTime -= 1000;
+  }
+  //console.log(currentTime / 1000)
+  //console.log(allEnemies[0].diffX(), allEnemies[0].diffY(), currentTime);
+  d3Enemies.each((d, i) => {
+    var px = d3player.attr('cx');
+    var py = d3player.attr('cy');
+    var pr = d3player.attr('r');
+
+    //console.log(px);
+    //console.log(py);
+
+    var enemyx = allEnemies[i].diffX();
+    var enemyy = allEnemies[i].diffY();
+    //var enemyx = allEnemies[i].lastx + (allEnemies[i].x - allEnemies[i].lastx) * (currentTime / 1000);
+    //if (currentTime === 100) console.log(enemyx, allEnemies[i].diffX());
+    //var enemyy = allEnemies[i].lasty + (allEnemies[i].y - allEnemies[i].lasty) * (currentTime / 1000);
+
+    // var dx = px - d.x;
+    // var dy = py - d.y;
+
+    var dx = px - enemyx;
+    var dy = px - enemyy;
+
+    if (-(2 * pr - 1) <= dx && dx <= (2 * pr - 1)) {
+      if (-(2 * pr) <= dy && dy <= (2 * pr)) {
+        collisionHandle();
+        //console.log('Collision detected', enemyx, px, dx, enemyy, py, dy)
+      }
+    }
+  });
+}, 10);
